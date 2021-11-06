9ae2a5a5fcd3b1e6cc68aba3fea0dd633e1fe4f4
askor26 <askorutin26@gmail.com>
2021-11-06 18:56:36 +0300

    some minor functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import 'bootstrap';
+import * as yup from 'yup';
 
 console.log('Hello World!');
+
+const urlSchema = yup.string().url();
+const form = document.querySelector('form.rss');
+console.log(form);
