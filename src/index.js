b62a0571ea9deceae3a8d5824d550479783f5714
askor26 <askorutin26@gmail.com>
2021-11-06 20:43:58 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap';
 import * as yup from 'yup';
 
-console.log('Hello World!');
+console.log('Hello World! new String');
 
 const urlSchema = yup.string().url();
 const form = document.querySelector('form.rss');
