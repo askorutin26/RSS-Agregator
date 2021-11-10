3cc4b8b953f69e2484789630977ccaa4e904b6bb
askor26 <askorutin26@gmail.com>
2021-11-10 20:51:39 +0300

    empty

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import 'bootstrap';
-import * as yup from 'yup';
 
 console.log('Hello World! new String');
 
-const urlSchema = yup.string().url();
-const form = document.querySelector('form.rss');
-console.log(form);
+console.log('form');
