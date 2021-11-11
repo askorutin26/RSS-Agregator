df6dfa972d948aca70b0134ddbda1bc17714bed6
askor26 <askorutin26@gmail.com>
2021-11-11 13:25:01 +0300

    test

--- src/urlValidator.js
+++ src/urlValidator.js
@@ -1,9 +1,6 @@
-import * as yup from 'yup';
-
 const validateURL = () => {
-  const schema = yup.string.url();
-  const form = document.querySelector('form.rss');
-  form.addEventListener('change', (e) => {
+  const input = document.querySelector('input .email');
+  input.addEventListener('change', (e) => {
     e.preventDefault();
     console.log('abobas');
   });
