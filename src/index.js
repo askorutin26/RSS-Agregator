5c4d923a008039868481dfdc3846dc2ecb54eee7
askor26 <askorutin26@gmail.com>
2021-11-11 17:01:07 +0300

    added import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import 'bootstrap';
-import validateURL from './urlValidator.js';
+import fn from './init.js';
 
-console.log('Hello World! new String');
-const input = document.querySelector('input.email');
-console.log(input);
-validateURL(input);
-
-console.log('form');
+fn();
