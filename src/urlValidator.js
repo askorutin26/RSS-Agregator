f8f661e6ab5a280d535b105d7d3edca204c3a93e
askor26 <askorutin26@gmail.com>
2021-11-11 17:01:34 +0300

    created funcction that validates url

--- src/urlValidator.js
+++ src/urlValidator.js
@@ -1,7 +1,10 @@
-const validateURL = (input) => {
-  input.addEventListener('change', (e) => {
-    e.preventDefault();
-    console.log('abobas');
-  });
+import * as yup from 'yup';
+
+const validateURL = (state) => {
+  const urlArr = state.formState.urls;
+  const url = state.formState.currentUrl;
+  const schema = yup.string().url().required().notOneOf(urlArr);
+  const valid = schema.isValidSync(url);
+  return valid;
 };
 export default validateURL;
