63a77e234a74453d3e62ebdebcb3010043aa058c
askor26 <askorutin26@gmail.com>
2021-11-14 15:50:08 +0300

    added i18next config and locales

---
+++ src/locales/locales.js
@@ -0,0 +1,21 @@
+export default {
+  en: {
+    translation: {
+      title: 'RSS Agregator',
+      description: 'Start reading Rss today!',
+      placeholder: 'RSS link',
+      addBtn: 'Add',
+      example: 'Example: https://ru.hexlet.io/lessons.rss',
+    },
+  },
+  ru: {
+    translation: {
+      title: 'RSS Аргегатор',
+      description: 'Начни читать RSS сегодня!',
+      placeholder: 'Ссылка RSS',
+      addBtn: 'Добавить',
+      example: 'Пример: https://ru.hexlet.io/lessons.rss',
+
+    },
+  },
+};
