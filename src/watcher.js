332d9fa9ce303ec367ad07ff14ccaeaea7557a4f
askor26 <askorutin26@gmail.com>
2021-11-22 18:10:32 +0300

    added watcher

---
+++ src/watcher.js
@@ -0,0 +1,27 @@
+import onChange from 'on-change';
+import renderForm from './view.js';
+import handler from './handler.js';
+import http from './ajax.js';
+
+const state = {
+  formState: {
+    currentURL: '',
+    previousURLS: [],
+    state: '',
+    valid: '',
+    error: '',
+  },
+};
+
+const app = () => {
+  const form = document.querySelector('.rss-form');
+  const container = document.querySelector('div.col-md-10');
+
+  const watchedState = onChange(state, (path, value, previousValue) => {
+    renderForm(container, state.formState);
+  });
+  handler(watchedState, form);
+  http(state.formState.currentURL);
+};
+
+export default app;
