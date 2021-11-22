c1a36d29ee6bce1bbc738ed6a7b8e10461356596
askor26 <askorutin26@gmail.com>
2021-11-22 18:05:16 +0300

    translation added

--- src/locales/locales.js
+++ src/locales/locales.js
@@ -6,16 +6,26 @@ export default {
       placeholder: 'RSS link',
       addBtn: 'Add',
       example: 'Example: https://ru.hexlet.io/lessons.rss',
+      validation: {
+        valid: 'RSS loaded!',
+        urlErr: 'Link must be valid URL',
+        alreadyExists: 'RSS already exists',
+      },
+
     },
   },
   ru: {
     translation: {
-      title: 'RSS Аргегатор',
-      description: 'Начни читать RSS сегодня!',
+      title: 'RSS Агрегатор',
+      description: 'Начните читать RSS сегодня!',
       placeholder: 'Ссылка RSS',
       addBtn: 'Добавить',
       example: 'Пример: https://ru.hexlet.io/lessons.rss',
-
+      validation: {
+        valid: 'RSS загружена!',
+        urlErr: 'Ссылка должна быть валидным URL',
+        alreadyExists: 'RSS уже добавлен',
+      },
     },
   },
 };
