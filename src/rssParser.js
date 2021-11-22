c48a7186ce971014607935218860b1dca498c59d
askor26 <askorutin26@gmail.com>
2021-11-22 20:54:20 +0300

    finished logic

--- src/rssParser.js
+++ src/rssParser.js
@@ -1,6 +1,27 @@
 const parseRSS = (xml) => {
   const domParser = new DOMParser();
   const doc = domParser.parseFromString(xml, 'text/html');
-  return doc;
+
+  const title = doc.querySelector('title').textContent;
+  const description = doc.querySelector('description').textContent;
+
+  const posts = Array.from(doc.querySelectorAll('item'));
+
+  const postElems = posts.map((post) => {
+    const postTitle = post.textContent;
+    const postLink = post.querySelector('link').textContent;
+    const postDescription = post.querySelector('description').textContent;
+    return {
+      title: postTitle,
+      link: postLink,
+      description: postDescription,
+    };
+  });
+
+  return {
+    title,
+    description,
+    postElems,
+  };
 };
 export default parseRSS;
