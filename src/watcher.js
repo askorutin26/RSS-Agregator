c53730d7c9ed968c457a18404921790e99aea712
askor26 <askorutin26@gmail.com>
2021-12-01 19:56:47 +0300

    refactored

--- src/watcher.js
+++ src/watcher.js
@@ -1,6 +1,6 @@
 import onChange from 'on-change';
-import { renderForm, renderFeedBlock } from './view.js';
-import handler from './handler.js';
+import { renderForm, renderFeedBlock, renderPostBlock } from './view.js';
+import formHandler from './handler.js';
 import http from './ajax.js';
 import parseRSS from './rssParser.js';
 
@@ -12,19 +12,37 @@ const state = {
     valid: '',
     error: '',
   },
+  feed: {
+    feedTitle: '',
+    feedDescription: '',
+  },
+  posts: [],
 };
-
 const app = () => {
   const form = document.querySelector('.rss-form');
+  const showBtn = document.querySelector('[data-bs-toggle="modal"]');
+  const modal = document.querySelector('.modal');
   const formContainer = document.querySelector('div.col-md-10');
-  const feedContainer = document.querySelector('div.order-0');
+  const feedContainer = document.querySelector('div.feeds');
+  const postsContainer = document.querySelector('div.posts');
 
   const watchedState = onChange(state, (path, value, previousValue) => {
-    renderForm(formContainer, state.formState);
-    const test = http(state.formState.currentURL);
-    test.then((response) => renderFeedBlock(feedContainer, parseRSS(response)));
+    switch (path) {
+      case 'formState.valid':
+        renderForm(formContainer, state.formState);
+        break;
+      case 'feed':
+        renderFeedBlock(feedContainer, watchedState.feed);
+        break;
+      case 'posts':
+        renderPostBlock(postsContainer, watchedState.posts);
+        break;
+      default:
+        break;
+    }
   });
-  handler(watchedState, form);
+  formHandler(watchedState, form);
+  console.log('1');
 };
 
 export default app;
