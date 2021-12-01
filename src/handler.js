7a3a9cff91493dee759069809e0a5f0956e70636
askor26 <askorutin26@gmail.com>
2021-12-01 19:55:45 +0300

    changed logic

--- src/handler.js
+++ src/handler.js
@@ -1,17 +1,51 @@
+import _ from 'lodash';
 import validateURL from './urlValidator';
+import makeQueryForRss from './ajax.js';
+import parseRSS from './rssParser';
 
-export default (state, element) => {
+const loadPosts = (state) => {
   const watchedState = state;
-  element.addEventListener('submit', (e) => {
+  const url = state.formState.currentURL;
+  makeQueryForRss(url).then((response) => {
+    const rssData = parseRSS(response.data);
+    const feedTitle = rssData.title;
+    const feedDescription = rssData.description;
+    const feedPosts = rssData.postElems;
+
+    const feed = {
+      feedTitle,
+      feedDescription,
+      id: _.uniqueId('feed_'),
+    };
+    const posts = feedPosts.map((post) => {
+      const id = _.uniqueId('post_');
+      return { id, post };
+    });
+    watchedState.feed = feed;
+    watchedState.posts = posts;
+    watchedState.formState.state = 'finished';
+  });
+};
+
+const formHandler = (state, elements) => {
+  const watchedState = state;
+  const form = elements;
+
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const input = document.querySelector('input');
     const { value } = input;
+
     const urlArr = watchedState.formState.previousURLS;
     watchedState.formState.currentURL = value;
+    watchedState.formState.state = 'processing';
+
     const validationResult = validateURL(value, urlArr);
     if (validationResult === 'valid') {
       watchedState.formState.valid = 'valid';
       watchedState.formState.previousURLS.push(value);
+      loadPosts(watchedState, elements);
     } else if (validationResult === 'this must be a valid URL') {
       watchedState.formState.error = 'urlErr';
       watchedState.formState.valid = 'invalid';
@@ -21,3 +55,4 @@ export default (state, element) => {
     }
   });
 };
+export default formHandler;
