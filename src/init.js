b929258615614f4302159eb8349cd9e64df4c42c
askor26 <askorutin26@gmail.com>
2021-12-19 18:08:06 +0300

    test

--- src/init.js
+++ src/init.js
@@ -6,4 +6,5 @@ const modalContainer = document.querySelector('div.modal.fade');
 const rssContainer = document.querySelector('.rss-container');
 const elements = [formContainer, modalContainer, rssContainer];
 console.log(document.outerHTML);
+console.log(document);
 app(elements);
