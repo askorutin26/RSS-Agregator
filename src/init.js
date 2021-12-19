4b0cef00674508bd110f3a6a48d4ae967d75db53
askor26 <askorutin26@gmail.com>
2021-12-19 18:21:12 +0300

    test

--- src/init.js
+++ src/init.js
@@ -1,5 +1,5 @@
 import app from './watcher.js';
-import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const formContainer = document.querySelector('.rss-form');
 const modalContainer = document.querySelector('div.modal.fade');
