64716276872a56992234ead465389dc6ec3a5ccf
askor26 <askorutin26@gmail.com>
2022-01-25 21:29:22 +0300

    revert changes

--- src/urlValidator.js
+++ src/urlValidator.js
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
-const validateURL = (normalizedUrl, state) => {
+function validateURL(normalizedUrl, state) {
   const existingUrls = state.feeds.map(({ url }) => url);
   const schema = yup.string().url().required().notOneOf(existingUrls);
   return schema.validate(normalizedUrl);
-};
+}
 export default validateURL;
