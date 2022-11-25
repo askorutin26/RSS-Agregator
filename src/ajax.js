a2c978eeee98a58e0b23643d2b3442398039414f
askor26 <askorutin26@gmail.com>
2022-11-25 16:11:47 +0300

    allorigins link updated

--- src/ajax.js
+++ src/ajax.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const wrapProxy = (url) => {
-  const urlBuilder = new URL('/get', 'https://hexlet-allorigins.herokuapp.com');
+  const urlBuilder = new URL('/get', 'https://allorigins.hexlet.app');
   urlBuilder.searchParams.set('url', url);
   urlBuilder.searchParams.set('disableCache', 'true');
   return urlBuilder.toString();
